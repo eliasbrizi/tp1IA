623f324040bb2c1e5d87243e5948bf47a0039dac
Elias <eliasbrizi@yahoo.es>
2020-05-06 20:41:34 -0300

    clases Go creadas

---
+++ inteligenciaArtificialTP1/src/coronaTracker/actions/GoC15.java
@@ -0,0 +1,39 @@
+package coronaTracker.Actions;
+import frsf.cidisi.faia.examples.search.robot.RobotAgentState;
+import java.util.ArrayList;
+import frsf.cidisi.faia.Agent.search.SearchAction;
+import frsf.cidisi.faia.Agent.search.SearchBasedAgentState;
+import frsf.cidisi.faia.state.AgentState;
+import frsf.cidisi.faia.state.EnvironmentState;
+public class GoC15 extends SearchAction {
+   @Override
+   public SearchBasedAgentState execute(SearchBasedAgentState s) {
+        CoronaTrackerAgentState agentState = (CoronaTrackerAgentState) s;
+       if (agentState.getVisitedPositions().contains(CoronaTrackerAgentState.C15)) {
+          return null;
+      }
+      ArrayList<String> successors = new ArrayList<String>();
+      successors.AddAll(agentState.getSuccessors());
+      if (successors != null) {
+          int index = successors.indexOf(CoronaTrackerAgentState.C15);
+          if (index >= 0) {
+              agentState.setPosition(CoronaTrackerAgentState.C15);
+           return agentState;
+         }
+   }
+     return null;
+  }
+   @Override
+   public EnvironmentState execute(AgentState ast, EnvironmentState est) {
+       this.execute((SearchBasedAgentState) ast);
+      return null;
+  }
+  @Override
+  public Double getCost() {
+      return new Double(0);
+  }
+@Override
+  public String toString() {
+      return "GoC15";
+  }
+}
---
+++ inteligenciaArtificialTP1/src/coronaTracker/actions/GoI0.java
@@ -0,0 +1,39 @@
+package coronaTracker.Actions;
+import frsf.cidisi.faia.examples.search.robot.RobotAgentState;
+import java.util.ArrayList;
+import frsf.cidisi.faia.Agent.search.SearchAction;
+import frsf.cidisi.faia.Agent.search.SearchBasedAgentState;
+import frsf.cidisi.faia.state.AgentState;
+import frsf.cidisi.faia.state.EnvironmentState;
+public class GoI0 extends SearchAction {
+   @Override
+   public SearchBasedAgentState execute(SearchBasedAgentState s) {
+        CoronaTrackerAgentState agentState = (CoronaTrackerAgentState) s;
+       if (agentState.getVisitedPositions().contains(CoronaTrackerAgentState.I0)) {
+          return null;
+      }
+      ArrayList<String> successors = new ArrayList<String>();
+      successors.AddAll(agentState.getSuccessors());
+      if (successors != null) {
+          int index = successors.indexOf(CoronaTrackerAgentState.I0);
+          if (index >= 0) {
+              agentState.setPosition(CoronaTrackerAgentState.I0);
+           return agentState;
+         }
+   }
+     return null;
+  }
+   @Override
+   public EnvironmentState execute(AgentState ast, EnvironmentState est) {
+       this.execute((SearchBasedAgentState) ast);
+      return null;
+  }
+  @Override
+  public Double getCost() {
+      return new Double(0);
+  }
+@Override
+  public String toString() {
+      return "GoI0";
+  }
+}
